f6e08c57049a138632d3303ef2216fd277605e04
Jarod Streckeisen <jarod.streckeisen@protonmail.ch>
2023-08-22 18:34:25 +0200

    add nodemon for hot reload

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("tamerfdp");
+  res.status(200).json({page:"Hello World"});
 });
 
 module.exports = router;
