1450a8ed0039e6787d7e155596ec8d7ffd1d0c59
Jarod <jarod.streckeisen@heig-vd.ch>
2023-08-28 14:25:05 +0200

    Middleware done

--- backend/middlewares/verify-jwt.js
+++ backend/middlewares/verify-jwt.js
@@ -1,6 +1,27 @@
-module.exports = (req,res,next) => {
-    console.log("this is middleware");
-    next();
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+/* 
+Middleware that verify that for every request a valid JWT is attached. 
+This verify that the request comes from an Authentified user
+*/
+module.exports = async (req,res,next) => {
+    // Verifier that expects valid access tokens:
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
+      tokenUse: "access",
+      clientId: process.env.AWS_COGNITO_CLIENT_ID,
+    });
+    
+    try {
+      const payload = await verifier.verify(
+        "eyJraWQeyJhdF9oYXNoIjoidk" // the JWT as string
+      );
+      console.log("Token is valid. Payload:", payload);
+      next();
+    } catch {
+      res.status(401).json({
+        error:"Unauthorized access"
+      })
+    }
 }
 
 
