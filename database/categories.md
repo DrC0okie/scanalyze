a37fd2a5403c52f02d880a3c54de9fd08f1da957
Anthony David <70233189+AnthonyDavid3110@users.noreply.github.com>
2023-09-01 00:46:15 +0200

    migros ok

---
+++ database/categories.md
@@ -0,0 +1,11 @@
+Les catégories définies :
+- Fruits et légumes "fruits-vegetables"
+- Viandes et poisson "meat-fish"
+- Boissons "drinks"
+- Produits laitiers et oeufs "dairies-eggs"
+- Pain et patisseries "bread"
+- Déjeuner "breakfast"
+- Congelés "frozen"
+- Plats préparés "convenience"
+- Snacks et sucrerie "snacks"
+- Pâtes, condiments et conservers "starches"
--- scrapper/src/workers/migros-worker.js
+++ scrapper/src/workers/migros-worker.js
@@ -2,21 +2,22 @@ import { workerData, parentPort } from 'worker_threads';
 import puppeteer from 'puppeteer';
 import { v4 } from 'uuid';
 let products = []
-console.log("URL : " + workerData.base_url);
-console.log("CAT : " + workerData.categories);
+// console.log("URL : " + workerData.base_url);
+// console.log("CAT : " + workerData.categories.name);
+console.log(workerData.categories.name)
 
 
 const run = async (cat) => {
     let has_more_product = true;
     const browser = await puppeteer.launch({
-        executablePath: "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe",
-        headless: "new"
+        executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+        headless: false
     });
     const page = await browser.newPage();
 
     await page.setViewport({ width: 1500, height: 1000 });
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4427.0 Safari/537.36');
-    await page.goto(workerData.base_url + "/" + cat)
+    await page.goto(workerData.base_url + "/" + cat.url)
     page.waitForNavigation();
     await page.exposeFunction("v4", v4);
 
@@ -29,30 +30,30 @@ const run = async (cat) => {
             has_more_product = false;
         });
     }
-    let actual_products = await page.evaluate(async () => {
+    let actual_products = await page.evaluate(async (cat) => {
         const items = document.getElementsByClassName("product-show-details");
         let actual_products = [];
         for (let i = 0; i < items.length; i++) {
             const item = items[i];
-            const actual_price = item.getElementsByClassName("actual").length ? parseFloat(item.getElementsByClassName("actual")[0].innerHTML.trim()) : 0
-            const regular_price = item.getElementsByClassName("regular-price").length ? item.getElementsByClassName("regular-price")[0].children[2].innerHTML : 0
+            const actual_price = item.getElementsByClassName("actual").length ? parseFloat(item.getElementsByClassName("actual")[0].innerHTML.trim()) : 0;
+            const regular_price = item.getElementsByClassName("regular-price").length ? item.getElementsByClassName("regular-price")[0].children[2].innerHTML : 0;
             let name = item.getElementsByClassName("name").length ? item.getElementsByClassName("name")[0].innerHTML + " " : "";
             for (let j = 0; j < item.getElementsByClassName("desc")[0].children.length; j++) {
                 name = name.concat(" ", item.getElementsByClassName("desc")[0].children[j].innerHTML || " ");
             }
             actual_products.push(
                 {
-                    id: await v4(),
                     actual_price,
                     regular_price,
-                    name
+                    name,
+                    category: cat.name
                 }
             )
 
 
         }
         return actual_products;
-    })
+    }, cat)
     browser.close();
 
     return actual_products;
@@ -60,7 +61,7 @@ const run = async (cat) => {
 
 for (let i = 0; i < workerData.categories.length; i++) {
     let prods = await run(workerData.categories[i]);
-    console.log("Finished fetching  " + prods.length + " product(s) in " + workerData.categories[i] + " category");
+    console.log("Finished fetching  " + prods.length + " product(s) in " + workerData.categories[i].name + " category");
     products = products.concat(prods);
 }
 
