b53f884d03036b5989872032666afcd334242c44
Anthony David <70233189+AnthonyDavid3110@users.noreply.github.com>
2023-09-07 01:05:52 +0200

    add video fb

---
+++ docs/video/video.md
@@ -0,0 +1,36 @@
+# Scanalyze - video
+
+Author : Anthony David
+
+Date :  07.09.2023
+
+Version : 1.0
+
+## Table of Contents
+[Introduction](#introduction)
+
+[Technologies](#technologies)
+
+[Right](#right)
+
+
+## Introduction
+Information about the video made for the presentation of the "Scanalyze" project.
+
+## Technologies
+### Voice generation
+- ElevenLabs
+### Animation
+- Adobe Animate
+- Adobe Characters Animator
+### Video editing
+- Adobe Premiere pro
+- Adobe Premiere Rush
+### Image processing
+- Adobe Photoshop
+- Adobe Illusrator
+### Text correction and structural assistance
+- Chat GPT
+
+## Rights
+All music and images uses in the video are royalty-free and come from free stock databases.
--- scrapper/src/workers/migros-worker.js
+++ scrapper/src/workers/migros-worker.js
@@ -1,69 +1,71 @@
-import { workerData, parentPort } from 'worker_threads';
+import {workerData, parentPort} from 'worker_threads';
 import puppeteer from 'puppeteer';
-import { v4 } from 'uuid';
-let products = [];
-console.log("URL : " + workerData.base_url);
+import {v4} from 'uuid';
 
-for (let i = 0; i < workerData.categories.length; i++){
-    console.log(workerData.categories[i].url + " --> " + workerData.categories[i].name);
-}
+let products = []
+console.log("URL : " + workerData.base_url);
+// console.log("CAT : " + workerData.categories);
 
 
 const run = async (cat) => {
     let has_more_product = true;
     const browser = await puppeteer.launch({
-        executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+        executablePath: "C:/Program Files/Google/chrome/Application/chrome.exe",
         headless: "new"
     });
     const page = await browser.newPage();
 
-    await page.setViewport({ width: 1500, height: 1000 });
+    await page.setViewport({width: 1500, height: 1000});
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4427.0 Safari/537.36');
-    await page.goto(workerData.base_url + "/" + cat.url)
+    await page.goto(workerData.base_url + "/" + cat)
     page.waitForNavigation();
     await page.exposeFunction("v4", v4);
 
+    // Display all the product of the category (by default only 100 are displayed)
     while (has_more_product) {
-        await page.waitForSelector(".mui-button-leshop:not([disabled])", { timeout: 10000 }).then(async (el) => {
+        await page.waitForSelector(".primary.m-button.mat-mdc-button-base:not([disabled]", {timeout: 10000}).then(async (el) => {
             has_more_product = true;
-            await el.click(".mui-button-leshop");
+            await page.evaluate(async () => {
+                await document.querySelector(".primary.m-button.mat-mdc-button-base").click();
+            })
 
         }).catch((error) => {
             has_more_product = false;
         });
     }
-    let actual_products = await page.evaluate(async (cat) => {
+
+    let actual_products = await page.evaluate(async () => {
         const items = document.getElementsByClassName("product-show-details");
         let actual_products = [];
+
         for (let i = 0; i < items.length; i++) {
             const item = items[i];
-            const actual_price = item.getElementsByClassName("actual").length ? parseFloat(item.getElementsByClassName("actual")[0].innerHTML.trim()) : 0;
-            const regular_price = item.getElementsByClassName("regular-price").length ? parseFloat(item.getElementsByClassName("regular-price")[0].children[2].innerHTML) : 0;
-            const weight_priceUnit = item.getElementsByClassName("weight-priceUnit ng-star-inserted")
+            const actual_price = item.getElementsByClassName("actual").length ? parseFloat(item.getElementsByClassName("actual")[0].innerHTML.trim()) : 0
+            const regular_price = item.getElementsByClassName("regular-price").length ? item.getElementsByClassName("regular-price")[0].children[2].innerHTML : 0
             let name = item.getElementsByClassName("name").length ? item.getElementsByClassName("name")[0].innerHTML + " " : "";
+            console.log(name);
             for (let j = 0; j < item.getElementsByClassName("desc")[0].children.length; j++) {
                 name = name.concat(" ", item.getElementsByClassName("desc")[0].children[j].innerHTML || " ");
             }
             actual_products.push(
                 {
+                    id: await v4(),
                     actual_price,
                     regular_price,
-                    name,
-                    category: cat.name
+                    name
                 }
             )
 
-
         }
         return actual_products;
-    }, cat)
+    })
     browser.close();
 
     return actual_products;
 }
 
 for (let i = 0; i < workerData.categories.length; i++) {
-    let prods = await run(workerData.categories[i]);
+    let prods = await run(workerData.categories[i].url);
     console.log("Finished fetching  " + prods.length + " product(s) in " + workerData.categories[i].name + " category");
     products = products.concat(prods);
 }
